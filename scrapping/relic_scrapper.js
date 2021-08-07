1edde2cc5fe0d16a9f2ade0d5442efa5141a88ea
Croquette the Soothsayer <Croquette@Soothsayers.universe>
2021-08-07 18:09:28 +0200

    feat(relic scrapping): skill parsing
    affined skill extraction.
    Missing "Stance" relics and ones with flat
    skill level bonuses

--- scrapping/relic_scrapper.js
+++ scrapping/relic_scrapper.js
@@ -9,25 +9,54 @@
 // @grant        none
 // ==/UserScript==
 
+const UNALLOWED_SKILL_NAMES = [
+    "Dexterity",
+    "Vitality",
+    "Energy",
+    "Strength",
+    "All Skills when using an Apple",
+    "Light Radius",
+    "Attack Rating"
+]
+
+function purge_skill_candidates(candidates) {
+    UNALLOWED_SKILL_NAMES.forEach(banned => {
+        let i = 0
+        while (i < candidates.length) {
+            if (candidates[i] == banned) {
+                candidates = candidates.splice(i, 1)
+            }
+            ++i
+        }
+    })
+    return candidates
+}
+
 function find_skill(attributes) {
     let regex = new RegExp("\\+\\(\\d+ to \\d+\\) to")
     let skill = undefined
+    let candidates = []
     attributes.forEach(line => {
         if (line.match(regex))
-            skill = line.split("to ")[2]
+            candidates.push(line.split("to ")[2])
     })
+    candidates = purge_skill_candidates(candidates)
+    if (candidates.length == 0)
+        console.log(attributes)
+    if (candidates.length > 1)
+        console.log(candidates)
     return skill
 }
 
 function parse_relic(node) {
     let text_container = node.children[2]
+    let name = text_container.children[0].innerText
     let attr = ""
     for (let i = 1; i < text_container.children.length; i++) {
         attr += text_container.children[i].innerText
     }
     let attributes = attr.split("\n")
     let skill = find_skill(attributes)
-    console.log("COTW " + skill)
 
 }
 
